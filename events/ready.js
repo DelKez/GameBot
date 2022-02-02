6df62ea49e8ec6b8e432878c4fe45a84274439d8
DelKez <47643868+DelKez@users.noreply.github.com>
2022-02-02 13:39:20 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('../config.json');
+const { clientId, guildId } = require('../config.json');
+const token = process.env.TOKEN;
 
 module.exports = {
 	name: 'ready',
